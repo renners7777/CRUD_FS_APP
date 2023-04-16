623cbb492c0bfc4e9558468103cff43f427d26c4
Chris <104800006+renners7777@users.noreply.github.com>
2023-04-16 18:10:16 +0100

    New dependencies installed, Mongodb connection established

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 console.log('May Node be with you')
 
 const express = require('express')
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
 const app = express()
+require("dotenv").config()
 
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
+MongoClient.connect(process.env.DB_STRING, (err, client) => {
+  if (err) return console.error(err);
+  console.log("Connected to Database");
+});
+
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`Server running on Port 8000...`);
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.post("/quotes", (req, res) => {
+    console.log(req.body);
+});
