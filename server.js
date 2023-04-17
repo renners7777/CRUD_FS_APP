5aa60f168348cc7f327ef1918b3e6a333c540815
Chris Renshaw <chrisrenshaw7@outlook.com>
2023-04-17 22:28:47 +0100

    Installed ejs + added views and index.ejs inside views

--- server.js
+++ server.js
@@ -9,27 +9,39 @@ const port = 3000
 const connectionString =
   "mongodb+srv://chrisrenshaw79:fdgByzMtYtALwH4H@star-wars-cluster.dsexylg.mongodb.net/?retryWrites=true&w=majority";
 
-        app.set("view engine", "ejs");
-
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then((client) => {
     console.log("Connected to Database")
     const db = client.db("star-wars-db")
     const quotesCollection = db.collection("quotes")
 
+        app.set("view engine", "ejs");
+
         app.use(bodyParser.urlencoded({ extended: true }))
-        app.post("/quotes", (req, res) => {
-        quotesCollection.insertOne(req.body).then((res) => {
-          res.redirect("/")
-        })
-         .catch(error => console.error(error))
-})
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
 
         app.get("/", (req, res) => {
-          res.sendFile(__dirname + "/index.html");
+          quotesCollection.find().toArray()
+          .then(results => {
+          console.log(results)
+          })
+          .catch(error => console.error(error))
+          res.sendFile(__dirname + "/index.html")
+          res.render('index.ejs', {})
         })
+
+        app.post('/quotes', (req, res) => {
+          quotesCollection.insertOne(req.body)
+          .then(res => {
+            return res.redirect('/')
+          })
+          .catch(error => console.error(error))
+        })
+        
+      
         app.listen(port, () => {
-          console.log(`listening on ${port}`);
+          console.log(`listening on port ${port}`);
         })
       })
           .catch((error) => console.error(error))
